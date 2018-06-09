a0c1119cf7fae841263aa8d231300b63e6bfee4d
Andrew Vala <38055092+awvala@users.noreply.github.com>
2018-06-09 17:22:44 -0500

    added clock object and logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,18 @@
+// Global variables
+var intervalId;
+var clockRunning = false;
+
+// Start game object
 var game = {
 
-    //global variables within game object
+    //game object variables
     correct: 0,
     incorrect: 0,
     outoftime: 0,
-    started: false,
-    finished: false,
     currentQuestion: 0,
     currentAnswer: "",
-    questionTimer: 15000,
-    answerTimer: 10000,
+    questionTimer: 15,
+    answerTimer: 10,
 
     slidearr: [
         // Array Config:  Question string, Answer 1 string, Answer 2 st4ring, Answer 3 string, Answwer 4 string, corrent answer number
@@ -30,42 +33,47 @@ var game = {
         ["Question 14", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 1],
     ],
 
-newQuestion: function (questionIndex) {
-// Hide previous answer id's and classes
-$("#results").css("display", "none");
-$(".answers").css("display", "inline-block");
-$("label").css("display", "inline-block");
-$("#nextButton").css("display", "none");
-$("#questionAndResponse").text(this.slidearr[questionIndex][0]);
+newQuestion: function () {
+    // Hide previous answer id's and classes
+    var questionIndex =  game.currentQuestion;
+    $("#results").css("display", "none");
+    $(".answers").css("display", "inline-block");
+    $("label").css("display", "inline-block");
+    $("#nextButton").css("display", "none");
+    $("#questionAndResponse").text(game.slidearr[questionIndex][0]);
 
-// Grab current Question slidearr and insert question and answers into id buttonBox
-for (i = 1; i < this.slidearr.length - 1; i++) {
-    var nextAnswer = "#label" + i;
-    $(nextAnswer).text(this.slidearr[questionIndex][i]);
-}
+// Grab current Question in slidearr and insert question and answers into buttonBox
+    for (i = 1; i < game.slidearr.length - 1; i++) {
+        var nextAnswer = "#label" + i;
+        $(nextAnswer).text(game.slidearr[questionIndex][i]);
+    }
 
 // update global variables currentQuestion and current Answer
-this.currentQuestion ++;
-game.currentAnswer = game.slidearr[questionIndex][5];
-console.log(this);
+    game.currentQuestion ++;
+    game.currentAnswer = game.slidearr[questionIndex][5];
 },
 
 showAnswer: function (userAnswer) {
-if (userAnswer == game.currentAnswer) {
-    game.correct ++;
-    $("#questionAndResponse").text("Correct!");
-} else {
-    game.incorrect ++;
-    $("#questionAndResponse").text("Incorrect!");
-}
+    if (userAnswer == game.currentAnswer) {
+        game.correct ++;
+        $("#questionAndResponse").text("Correct!");
+    } else {
+        game.incorrect ++;
+        $("#questionAndResponse").text("Incorrect!");
+    }
+
 // then clear id buttonBox of question and display answers for 10 seconds
+    $('input[name=quizAnswer]').prop('checked', false);
     $(".answers").css("display", "none");
     $("#submitButton").css("display", "none");
     $("#nextButton").css("display", "inline-block");
-    game.currentQuestion ++;
-// If currentQuestions is less than 15 Increment currentQuestions global variable, run newQuestion function
-// if currentQuestion is greater than 14, run showResults function
-
+    
+    if (game.currentQuestion < 15) {
+        //start 10 second timer
+        answerClock.start(game.answerTimer);
+    } else {
+        game.showResults();
+    }
 },
 
 showResults: function () {
@@ -74,13 +82,62 @@ showResults: function () {
 // show startButton and change value attrribute to "try again?""
 },
 
-}; // end of game object
+}; 
+// End of game object
+
+// Start timer object
+var answerClock = {
+
+    // timer object variables
+    time: "",
+
+    start: function (newTime) {
+        if (!clockRunning) {
+            answerClock.time = newTime;
+            //$("#timerText").css("display", "inline-block");
+            intervalId = setInterval(answerClock.count, 1000);
+            clockRunning = true;
+            $("#timerText").text("Next question in " + newTime + " seconds");
+          }
+    },
+
+    stop: function() {
+        clearInterval(intervalId);
+        clockRunning= false;
+        $("#timerText").text("");
+        game.newQuestion();
+      }, 
+
+    count: function() {
+        answerClock.time--;
+        var currentTime = answerClock.time;
+        if (currentTime > 1) {
+            var endString = " seconds!";
+        } else if (currentTime == 1) {
+            var endString = " second!";
+        } else {
+            answerClock.stop();
+        }
+        $("#timerText").text("Next question in " + currentTime + endString);
+    },
+
+    /*count: function() {
+        answerClock.time--;
+        var currentTime = answerClock.time;
+        if (currentTime > 1) {
+            var endString = "seconds left!";
+        } else {
+            var endString = "second left!";
+        }
+        $("#timerText").text("You have " + currentTime + endString);
+    },*/
+};
+// End timer object
 
 $(document).ready(function() {
 
     //On startButton click
     $(".startButton").click(function() {
-        game.started = true;
         $("#bottomBanner").css("display", "none");
         $("#startMessage").css("display", "none");
         $(".startButton").css("display", "none");
@@ -89,7 +146,7 @@ $(document).ready(function() {
         game.outoftime = 0;
         game.currentQuestion = 0;
         game.currentAnswer = "";
-        game.newQuestion(0);
+        game.newQuestion();
     });
 
     // Display submit button after a radio button is clicked
@@ -101,7 +158,6 @@ $(document).ready(function() {
     $("#submitButton").click(function() {
         $("#submitButton").css("display", "none");
         var userAnswer = $('input[name=quizAnswer]:checked').val();
-        game.currentQuestion ++;
         game.showAnswer(userAnswer);
     });
 
