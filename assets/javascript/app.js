1b55f5324c851a4e8854f5521b7126ed718434d8
Andrew Vala <38055092+awvala@users.noreply.github.com>
2018-06-09 08:59:26 -0500

    initial JS variables

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+var game = {
+
+    correct: 0,
+    incorrect: 0,
+    outoftime: 0,
+    started: false,
+
+    slidearr: [
+        ["Question 0", "Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        ["Question 1", "Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        ["Question 2", "Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        ["Question 3", "Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        ["Question 4", "Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        ["Question 5", "Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        ["Question 6", "Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        ["Question 7", "Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        ["Question 8", "Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        ["Question 9", "Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        ["Question 10", "Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        ["Question 11", "Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        ["Question 12", "Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        ["Question 13", "Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        ["Question 14", "Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+    ],
+
+};
