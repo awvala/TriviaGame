a5706eead60c2f8af8bef6557f26b768d8bb5eb7
Andrew Vala <38055092+awvala@users.noreply.github.com>
2018-06-10 13:32:03 -0500

    Added questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,22 +17,22 @@ var game = {
     answerTimer: 10,
 
     slidearr: [
-        // Array Config:  Question string, Answer 1 string, Answer 2 st4ring, Answer 3 string, Answwer 4 string, current answer number, gif link
-        ["Question 0", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 2, "assets/images/kyudo0.gif"],
-        ["Question 1", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 3, "https://j.gifs.com/L8pZBA.gif"],
-        ["Question 2", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 1, "assets/images/kyudo2.gif"],
-        ["Question 3", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 4, "https://j.gifs.com/rRG2V4.gif"],
-        ["Question 4", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 3, "assets/images/kyudo4.gif"],
-        ["Question 5", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 4, "https://j.gifs.com/2vp8D1.gif"],
-        ["Question 6", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 2, "assets/images/kyudo6.gif"],
-        ["Question 7", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 3, "assets/images/kyudo7.gif"],
-        ["Question 8", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 4, "assets/images/kyudo8.gif"],
-        ["Question 9", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 1, "assets/images/kyudo9.gif"],
-        ["Question 10", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 1, "assets/images/kyudo10.gif"],
-        ["Question 11", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 4, "assets/images/kyudo11.gif"],
-        ["Question 12", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 3, "assets/images/kyudo12.gif"],
-        ["Question 13", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 2, "assets/images/kyudo13.gif"],
-        ["Question 14", "Answer 1", "Answer 2", "Answer 3", "Answer 4", 1, "https://j.gifs.com/rRG2B4.gif"],
+        // Array Config:  Question string, Answer 1 string, Answer 2 string, Answer 3 string, Answwer 4 string, correct answer index number, gif link
+        ["What is Kyudo?", "A book by Stephen King", "The way of the bow", "Greek for praise", "A Japanese sports festival", 2, "assets/images/kyudo0.gif"],
+        ["What are the three supreme goals of Kyudo?", "Discipline, A Warrior Spirit, Health", "Cleanliness, Respect, Service", "Truth, Goodness, Beauty", "Zen, Maintenance, Readiness", 3, "https://j.gifs.com/L8pZBA.gif"],
+        ["What is yugake?", "A shooting glove", "Kyudo uniform", "Archery target", "The eight steps for correct shooting", 1, "assets/images/kyudo2.gif"],
+        ["What is the Japanese term for a bow?", "Kyu", "Kikyo", "Yomi", "Yumi", 4, "https://j.gifs.com/rRG2V4.gif"],
+        ["What is the Japanese term for an arrow?", "Dozo", "Nigiri", "Ya", "Itatsuki 4", 3, "assets/images/kyudo4.gif"],
+        ["What is tsurune?", "The swell of pride upon hitting the target", "The process for nocking an arrow", "The top of the bow which connects the string", "The sound made by the bowstring on release", 4, "https://j.gifs.com/2vp8D1.gif"],
+        ["What should the bow do after the arrow is released?", "The bow should remain firm and unmoving in the left hand", "The bow should spin naturaly in the left hand", "The bow should angle to the floor at a 45 degree angle", "The bow should move slightly upwards to give the arrow greater range", 2, "assets/images/kyudo6.gif"],
+        ["To achieve maximum force on releasing the arrow you should", "Use the longest, curviest bow you can hold", "Pull the string back as far as your strength allows", "Equally push the bow and pull the string along a straight line", "Use a carbon fiber bow", 3, "assets/images/kyudo7.gif"],
+        ["The term Hikiwaki refers to the...", "Ceremonial process for approaching the target", "Beauty of shooting", "Truth in the arrow finding its target", "Drawing apart the bow", 4, "assets/images/kyudo8.gif"],
+        ["How many arrows are held on the first shot?", "Two", "Always one", "None, the first shot is practice", "Three", 1, "assets/images/kyudo9.gif"],
+        ["Select the correct order of the Hassetsu", "Ashibumi, Dozukuri, Yugamae, Uchiokoshi, Hikiwake, Kai, Hanare, Zanshin", "Ashibumi, Dozukuri, Uchiokoshi, Yugamae, Hikiwake, Kai, Hanare, Zanshin", "Dozukuri, Uchiokoshi, Yugamae, Kai, Hikiwake, Ashibumi, Zanshin, Hanare", "Hikiwake, Ashibumi, Dozukuri, Uchiokoshi, Yugamae, Kai, Hanare, Zanshin", 1, "assets/images/kyudo10.gif"],
+        ["Yabusame refers to which type of Kyudo?", "Ceremonial", "Combat", "Demonstration", "Mounted", 4, "assets/images/kyudo11.gif"],
+        ["Which of the following is NOT a piece of Kyudogi?", "Tabi", "Hakama", "Hana", "Yugake", 3, "assets/images/kyudo12.gif"],
+        ["Kyudoka aim the arrow by...", "Adjusting the angle of the left arm", "They don't aim the arrow, they focus on the Hassetsu", "Adjusting the angle of the right arm", "Adjusting both arms", 2, "assets/images/kyudo13.gif"],
+        ["The Japanese term for the target is", "Mato", "Buru", "Taiyo", "Doro", 1, "https://j.gifs.com/rRG2B4.gif"],
     ],
 
 newQuestion: function () {
